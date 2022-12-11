0b32a339666e4b0438940f18f694d296d9e22f53
AliReZa Sabouri <alirezanet@outlook.com>
2022-12-11 16:44:15 +0330

    refactor: use GetKeyValues in  get All permissions

--- FlexibleAuth/Shared/Authorization/PermissionsProvider.cs
+++ FlexibleAuth/Shared/Authorization/PermissionsProvider.cs
@@ -7,9 +7,6 @@ public static class PermissionsProvider
 {
     public static Dictionary<string,Flag<Permission>> GetAll()
     {
-        return typeof(Permission)
-            .GetFields(BindingFlags.Public | BindingFlags.Static)
-            .Where(f => f.FieldType == typeof(Flag<Permission>))
-            .ToDictionary(f => f.Name, f => (Flag<Permission>) f.GetValue(null)!);
+        return Permission.GetKeyValues();
     }
 }
